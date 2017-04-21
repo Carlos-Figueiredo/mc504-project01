981d0a6ccab4edc73256985ae2944dd4d67a84ca
Ruy Castilho Barrichelo <ruy.castilho26@gmail.com>
2017-04-21 15:37:40 -0300

    Finally works.

--- cigar.c
+++ cigar.c
@@ -4,7 +4,7 @@
   was based on the solution for 3 threads, presented in the "Little Book Of
   Semaphores", by Allen B. Downey.
  */
-#include <pthread.h>
+ #include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,12 +12,11 @@
 
 #define N 10                 		// Number of ingredients
 
-pthread_mutex_t agentMutex;         // Only one agent may act each cycle
-pthread_mutex_t pusherMutex[N];     // Act as barriers until agents make
+sem_t agentMutex;         	// Only one agent may act each cycle
+sem_t pusherMutex[N];     		// Act as barriers until agents make
 									// the corresponding ingredient available
-pthread_mutex_t smokerMutex[N];		// Only one smoker may act each cycle.
-pthread_mutex_t pusherLock;			// Only one pusher may act at a time
-pthread_cond_t cond_pusher[N], cond_smoker[N];// Signaling conditions.
+sem_t smokerMutex[N];			// Only one smoker may act each cycle.
+sem_t pusherLock;			// Only one pusher may act at a time
 
 int ingredients[N]; 				// Informs which ingredients are available
 
@@ -27,18 +26,18 @@ void* agentN(void *v);
 void* smokerN(void *v);
 
 int main() {
-	//Initializing ingredients, mutexes and condition variables.
+	//Initializing ingredients and semaphores.
+	sem_init(&pusherLock, 0, 1);
+	sem_init(&agentMutex, 0, 1);
+
 	for (int i = 0; i < N; i++) {
 	  	ingredients[i] = 0;
-		pthread_cond_init(&cond_pusher[i], 0);
-		pthread_cond_init(&cond_smoker[i], 0);
-		pthread_mutex_init(&pusherMutex[i], 0);
-		pthread_mutex_init(&smokerMutex[i], 0);
+		sem_init(&smokerMutex[i], 0, 0);
+		sem_init(&pusherMutex[i], 0, 0);
+
 	}
 
-	pthread_mutex_init(&pusherLock, 0);
-	pthread_mutex_init(&agentMutex, 0);
-
+	
 	// Alocating and identifying N threads
 	pthread_t agentThread[N], pusherThread[N], smokerThread[N];
 	int i, id[N];
@@ -64,14 +63,12 @@ void* agentN(void *v) {
 	int thisId = *(int *) v;
 
 	while(1){
-   		// The agent that escapes will release all ingredient but one
-		pthread_mutex_lock(&agentMutex);
-
-		printf("\n\nAGENT %d\n\n", thisId);
+   		// The agent that escapes will release all ingredients but one
+		sem_wait(&agentMutex);
 
     	for(int i = 0; i < N; i++)
 		  if(i != thisId)
-		    pthread_cond_signal(&cond_pusher[i]); // Releases the ingredients
+		    sem_post(&pusherMutex[i]); // Releases the ingredients
 
 
 	}
@@ -82,21 +79,19 @@ void* pusherN(void *v) {
 	int thisId = *(int *) v;
 
   	while(1){
-		pthread_cond_wait(&cond_pusher[thisId], &pusherMutex[thisId]);
+		sem_wait(&pusherMutex[thisId]);
 
-		pthread_mutex_lock(&pusherLock);
+		sem_wait(&pusherLock);
 
 		// Sets ingredient as available
 		ingredients[thisId] = 1;
 
-		printf("\n\nPUSHER %d\n\n", thisId);
 		int missingIngredient = 0;
 		int amountMissing = 0;
 
 		// Each pusher will check if the information of which ingredient is the one
 		// missing is available, for every ingredient
 		for(int i = 0; i < N && amountMissing <= 1; i++){
-			//printf("\n\ni %d\n\n", amountMissing);
 	 		if (ingredients[i] == 0) {
 	    		missingIngredient = i;
 	    		amountMissing += 1;
@@ -107,7 +102,6 @@ void* pusherN(void *v) {
 		// If there is only one ingredient missing, the corresponding smoker is
 		// signaled.
 	  	if (amountMissing == 1) {
-			//printf("\n\nMISSING %d\n\n", missingIngredient);
 	  		// Resets the ingredients list.
 		  	for(int i = 0; i < N ; i++){
 				ingredients[i] = 0;
@@ -115,12 +109,12 @@ void* pusherN(void *v) {
 		  	}
 
 			// Signals to the correct smoker.
-		 	pthread_cond_signal(&cond_smoker[missingIngredient]);
+		 	sem_post(&smokerMutex[missingIngredient]);
 
 		}
 
-		pthread_mutex_unlock(&pusherLock);
-		pthread_mutex_unlock(&pusherMutex[thisId]);
+		sem_post(&pusherLock);
+		sem_post(&pusherMutex[thisId]);
   	}
 
 	return NULL;
@@ -130,14 +124,12 @@ void* smokerN(void *v) {
     int thisId = *(int *) v;
 
     while(1){
-     	 pthread_cond_wait(&cond_smoker[thisId], &smokerMutex[thisId]);
+     	sem_wait(&smokerMutex[thisId]);
 
-     	 printf("\nSmoker %d made cigarrette.\n", thisId);
-
-		pthread_mutex_unlock(&smokerMutex[thisId]);
+     	printf("\nSmoker %d made cigarrette.\n", thisId);
 		
 		// Signals agentMutex, to allow a new cycle.
-		pthread_mutex_unlock(&agentMutex);
+		sem_post(&agentMutex);
     }
 
 }
